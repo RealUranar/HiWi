57ead28ab5b729f8228289e478779f2dcc05d2a7
RealUranar <uranar@gmx.de>
2023-10-23 09:27:50 +0200

    Revert "whatever happend here"
    This reverts commit cb6df9074bb017208719032a7435c0d4be3541d7.

---
+++ Program/Modules/Tasks/Gaussian.py
@@ -0,0 +1,155 @@
+import os, shutil, glob, sys
+
+sys.path.append("Modules/Misc")
+from InputFileReader import Reader
+from Sbatch import JobScripts
+from task import Task
+
+from rdkit import Chem
+from rdkit.Chem.rdDetermineBonds import DetermineBonds
+from rdkit.Chem.rdMolTransforms import SetDihedralDeg
+from rdkit.Chem.rdmolfiles import MolToXYZBlock
+from rdkit.Chem import AllChem
+
+class MoleculeActions():
+    def combineMolecules(mol1, mol2, index:tuple[int,int]):  #Takes the rdkit mols and combines them
+        """Function to combine two rdkit-mol objects at a specific point
+
+        Args:
+            mol1 (_mol_): rdkit structure of molecule Nr.1
+            mol2 (_mol_): rdkit Structure of molecule Nr.2
+            index (_tuple_): A tuple with the index of the atoms to connect (eg. (12, 24)). IMPORTANT: The order has to match the order of the given Molecules
+
+        Returns:
+            _mol_: A New Molecule
+        """
+        combo = Chem.CombineMols(mol1, mol2)  #Combine
+        edcombo = Chem.EditableMol(combo)     #Make editabel
+        a1, a2 = index
+        
+        edcombo.AddBond(a1, mol1.GetNumAtoms()+ a2, order=Chem.rdchem.BondType.SINGLE)  #Combine the molecules with a bond
+        combinedMol = edcombo.GetMol()  #Get the finished structure
+
+        combinedMol.UpdatePropertyCache() #Black magic to make the structure work
+        Chem.GetSymmSSSR(combinedMol)
+
+        return Molecule(combinedMol)
+    
+    def Mol2COM(mol): #Takes the rdkit mol and give .com file
+        from openbabel import openbabel
+        obConversion = openbabel.OBConversion()
+        obConversion.SetInAndOutFormats("mol", "com")
+
+        molFile =Chem.MolToMolBlock(mol)
+        comFile = openbabel.OBMol()
+        obConversion.ReadString(comFile, molFile)
+        return obConversion.WriteString(comFile)
+    
+
+class Molecule():
+    def __init__(self, molecule):
+        if type(molecule) == str:
+            self.mol = Chem.MolFromXYZFile(molecule)
+            DetermineBonds(self.mol,charge = 0)
+        else:
+            self.mol = molecule
+    
+    def getMol(self):
+        return self.mol
+    
+    def removeAtom(self, atomNr):
+        edFrag = Chem.EditableMol(self.mol)
+        edFrag.RemoveAtom(atomNr)
+        self.mol = edFrag.GetMol()
+    
+
+
+class Gaussian_opt(Task,Reader):
+    def __init__(self,job):
+        super().__init__(job)
+        self.newPath = f"{self.job.location}Gaussian"
+        self.inputFile = ""
+        self.executionOrder = [self.moveFiles,
+                               self.writeInputFile,
+                               self.generateJobScript,
+                               self.submit]
+
+    def moveFiles(self):
+        os.mkdir(f"{self.job.location}/Gaussian") # Create new SubFolders
+        optiFilePath = glob.glob(f"{self.job.location}/Orca_Opt/orca.xyz")[0]  #Get Path to the optimized structure
+        shutil.copy(optiFilePath, f"{self.newPath}/orca_opt.xyz") #Copy xyz File to new directory
+
+
+    def writeInputFile(self):
+        comFile = self._setupMolecule()
+        with open(f"{self.newPath}/combined.com", "w") as file:
+                    file.writelines([
+                        "%Chk=checkpoint.chk\n",
+                        "#P RHF/6-31G* Opt",
+                        "\n\nCOMMENT\n\n"])
+
+                    for line in list(comFile.split("\n"))[5:]:
+                        file.write(f"{line}\n")
+
+                    file.writelines([
+                        "--Link1--\n",
+                        "%Chk=checkpoint.chk\n",
+                        "#P HF/6-31G* SCF=Tight Geom=AllCheck Guess=Read\n",
+                        "Pop=MK IOp(6/33=2, 6/41=10, 6/42=17)\n"
+                    ])
+
+    def generateJobScript(self):
+        JobScripts().writeGausianJob(name = self.job.id, location=self.newPath)
+
+    def submit(self):
+        self.job.updateJob(Gaussian = 2)
+        print(f"Submitted Gaussian job {self.job.name}")
+        return super().submit(self.newPath)
+        
+    def isFinished(self):
+        hasFinished, succesfull = False, True
+        tail = self._readTail(self.newPath, "*.log")
+        hasFinished = "Normal termination of Gaussian" in tail
+        #succesfull = "****ORCA TERMINATED NORMALLY****" in str(tail)
+            
+        if hasFinished:
+            if succesfull:
+                self.job.updateJob(Gaussian = 1, Amber = 3)
+                print(f"Gaussian Job {self.job.name} has finished succesfull")
+            else:
+                self.job.updateJob(Gaussian = -1)
+                print(f"Gaussian Job {self.job.name} run into a problem")
+        else:
+            print(f"Gaussian Job {self.job.name} is still running")
+
+
+    def _setupMolecule(self):
+        inputVars = self.readInputFile(f"{self.job.location}Input")
+
+        molecule = Molecule(f"{self.newPath}/orca_opt.xyz")
+        molecule.removeAtom(inputVars["removeAtomNr"]-1)
+
+        fragment = Molecule(f"Modules/fragment.xyz")
+        fragment.removeAtom(inputVars["removeAtomFragmentNr"]-1)  #Everywhere -1 because index starts at 0
+        
+
+        combinedMolecules = MoleculeActions.combineMolecules(fragment.getMol(), molecule.getMol(), (inputVars["combineFragmentAt"]-1,inputVars["combineAtomAt"]-1))
+        AllChem.ConstrainedEmbed(combinedMolecules.mol, fragment.mol)  #Somewhat relax the structure to make a belivable Molecule
+
+        di = self._findSubstring(smilesString="CN=NC", inStructure= MolToXYZBlock(combinedMolecules.getMol()))[0]
+        SetDihedralDeg(combinedMolecules.mol.GetConformer(), di[0], di[1], di[2], di[3], 180) #Rotate the molecule to the right dihedral
+
+        comFile = MoleculeActions.Mol2COM(combinedMolecules.getMol())
+        return comFile
+
+if __name__ == "__main__":
+    sys.path.append("Modules/Misc")
+    from job import Job
+    job = Job(name = "Test", id = 666, location="Calculations/TESTING/", tasks={"Gaussian":1})
+    
+    task = Gaussian_opt(job)
+    task.writeInputFile()
+    #task._setupMolecule()
+    #task.moveFiles()
+    #task.generateJobScript()
+    #task.submit()
---
+++ Program/Modules/Tasks/GromacsEnergy.py
@@ -0,0 +1,110 @@
+import os, shutil, glob
+import subprocess
+from task import Task
+
+class GromacsEnergy(Task):
+    def __init__(self,job):
+        super().__init__(job)
+        self.newPath = f"{self.job.location}Gromacs"
+        self.executionOrder = [self.moveFiles,
+                               self.writeInputFile,
+                               self.generateJobScript,
+                               self.submit]
+
+    def moveFiles(self):
+        os.mkdir(self.newPath) # Create new SubFolders
+        try:
+            groFilePath = glob.glob(f"{self.job.location}Amber/*.gro")[0]
+            topFilePath = glob.glob(f"{self.job.location}Amber/*.top")[0]
+        except IndexError:
+            print(f"gro/top-File not Found for Job {self.job.name}")
+            raise FileNotFoundError
+
+        shutil.copy(groFilePath, f"{self.newPath}/System.gro")
+        shutil.copy(topFilePath, f"{self.newPath}/System.top")
+
+
+    def writeInputFile(self):
+        self._changeGROFile()
+        self._changeTOPFile()
+        self._writePosRe()
+        shutil.copy("Modules/GromacsScripts/em.mdp", f"{self.newPath}")
+
+    def generateJobScript(self):
+        with open(f"{self.newPath}/em.sh","w") as file:
+            file.writelines([
+            "#!/usr/local_rwth/bin/zsh\n",
+            "module load GCC/11.2.0 OpenMPI/4.1.1 GROMACS/2021.5-PLUMED-2.8.0\n",
+            "gmx grompp -f em.mdp -c System.gro -p System.top -r System.gro -o em.tpr\n",
+            "gmx mdrun -v -deffnm em -ntmpi 1\n"
+            ])
+        os.chmod(f"{self.newPath}/em.sh", 0o755)
+
+
+    def submit(self):
+        ret = subprocess.run(f"./em.sh",
+                    capture_output = True, 
+                    text = True,
+                    cwd=self.newPath)
+        
+        if ret.returncode != 0:
+             self.job.updateJob(GromacsEnergy = -1)
+        else: 
+            self.job.updateJob(GromacsEnergy= 1, GromacsEquil= 3)
+        print(f"Gromacs energy minimization returned code: {ret.returncode}")
+        
+
+
+    def _writePosRe(self):
+        with open(f"{self.job.location}Amber/System.gro","r") as file:
+            structure = file.read()
+        freezeAtoms = self._findSubstring(smilesString="CSC" ,inStructure=structure, inFormat="gro")[0]
+
+        with open(f"{self.newPath}/posre.itp", "w") as file:
+            file.writelines([
+                "[ position_restraints ]\n",
+                "; atom  type      fx      fy      fz\n"])
+            for atom in freezeAtoms:
+                file.writelines([
+                    f"{atom+1}      1       1000000    1000000    1000000\n"
+                ])
+
+    
+    def _changeTOPFile(self):
+        with open(f"{self.newPath}/System.top", "r") as file:
+            lines = file.readlines()
+
+        temp = []
+        for i, line in enumerate(lines):
+            if "[ bonds ]" in line:
+                temp.insert(i-1, '\n#ifdef POSRES\n#include "posre.itp"\n#endif\n')
+            temp.append(line)
+
+        with open(f"{self.newPath}/System.top", "w") as file:
+            file.writelines(temp)
+
+    def _changeGROFile(self):
+        with open(f"{self.newPath}/System.gro", "r") as file:
+            groFile = file.readlines()
+
+        groFile[-1] = "2.40000   2.40000   5.00000\n"
+
+        with open(f"{self.newPath}/System.gro", "w") as file:
+            file.writelines(groFile)
+
+
+if __name__ == "__main__":
+    import sys
+    sys.path.append("Modules/Misc")
+    from job import Job
+    job = Job(name = "Test", id = 666, location="Calculations/TESTING/", tasks={"Amber":1})
+    
+    task = GromacsEnergy(job)
+    out = task._changeGROFile()
+    print(out)
+    # task._writePosRe()
+    # task._writeTableFourier()
+    #task.moveFiles()
+    #task.writeInputFile()
+    #task.generateJobScript()
+    # task.submit()
