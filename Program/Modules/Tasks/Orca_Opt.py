4d14bb1dcf4e9cb9d4ff101bff46a9e3c92e0043
RealUranar <lukas.manuel.seifert@gmx.de>
2023-11-14 17:02:32 +0100

    go

--- Program/Modules/Tasks/Orca_Opt.py
+++ Program/Modules/Tasks/Orca_Opt.py
@@ -17,7 +17,7 @@ class Orca_opt(Task):
     def moveFiles(self):
         print(self.newPath)
         os.mkdir(self.newPath) # Create new SubFolders
-        xyzFilePath = glob.glob(f"{self.job.location}*.xyz")  #Get Path to the xyz File
+        xyzFilePath = list(set(glob.glob(f"{self.job.location}*.xyz")) - set(glob.glob(f"{self.job.location}fragment.xyz"))) #Get Path to the xyz File
         xyzFilePath.remove("fragment.xyz")  #Remove the fragment file
         shutil.copy(xyzFilePath[0], f"{self.newPath}/startMolecule.xyz") #Copy xyz File to new directory
         os.remove(xyzFilePath[0])
